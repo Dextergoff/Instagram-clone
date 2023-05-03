e8788eced0e4ec007a340de3f86e65a63c0668bb
Dextergoff <dextergoff571@gmail.com>
2023-05-03 16:08:07 -0700

    delete unused div

--- frontend/client/src/components/Image/PostImage.js
+++ frontend/client/src/components/Image/PostImage.js
@@ -1,12 +1,10 @@
 const PostImage = (props) => {
   return (
-    <div>
-      <img
-        className="post-image"
-        style={props.style}
-        src={process.env.REACT_APP_API_URL + props.image}
-      />
-    </div>
+    <img
+      style={props.style}
+      className="post-image"
+      src={process.env.REACT_APP_API_URL + props.image}
+    />
   );
 };
 export default PostImage;
