de0c406a851a19a9544aae856c437e7a234d0a5a
Dextergoff <dextergoff571@gmail.com>
2023-05-03 12:25:41 -0700

    remove date

--- frontend/client/src/components/posts/TitleAndHashtags.js
+++ frontend/client/src/components/posts/TitleAndHashtags.js
@@ -1,18 +1,24 @@
-import getDate from "components/jobs/getDate"
 const TitleAndHashtags = (prop) => {
-    const post = prop.post
-    return (
-        <div className="d-flex gap-2 mb-2">
-        <div className="text-light">{getDate(post.date)}</div>
-        <div className="text-muted">•</div>
-        <div className="text-center fw-bold  text-light ">{post.user.username}</div>
-        <div className="fw-light text-light">{post.title}</div>
-        
-        {post.hashtags.map((hashtag, index) => (
-          <a href={`/h/${hashtag}`} key={index} style={{textDecoration:"none"}} className="text-primary text">#{hashtag}</a>
-        ))}
+  const post = prop.post;
+  return (
+    <div className="d-flex gap-2 mb-2">
+      <div className="text-center fw-bold  text-light ">
+        {post.user.username}
       </div>
-    )
-}
+      <div className="fw-light text-light">{post.title}</div>
 
-export default TitleAndHashtags
+      {post.hashtags.map((hashtag, index) => (
+        <a
+          href={`/h/${hashtag}`}
+          key={index}
+          style={{ textDecoration: "none" }}
+          className="text-primary text"
+        >
+          #{hashtag}
+        </a>
+      ))}
+    </div>
+  );
+};
+
+export default TitleAndHashtags;
