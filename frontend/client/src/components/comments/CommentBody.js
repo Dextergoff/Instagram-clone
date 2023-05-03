85c21ddc8e259b11dd33e09361548942f116d94f
Dextergoff <dextergoff571@gmail.com>
2023-05-03 09:51:06 -0700

    comment components

---
+++ frontend/client/src/components/comments/CommentBody.js
@@ -0,0 +1,9 @@
+const CommentBody = ({ data }) => {
+  return (
+    <div style={{ width: "24vw" }} className="text-light">
+      {data}
+    </div>
+  );
+};
+
+export default CommentBody;
---
+++ frontend/client/src/components/comments/CommentUsername.js
@@ -0,0 +1,5 @@
+const CommentUsername = ({ data }) => {
+  return <div className=" mr-auto fw-bold text-light">{data.username}</div>;
+};
+
+export default CommentUsername;
