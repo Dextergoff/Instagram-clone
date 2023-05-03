ceeb03fbfd92a50239abd64031a483cf73968cfa
Dextergoff <dextergoff571@gmail.com>
2023-05-03 12:25:48 -0700

    add date

--- frontend/client/src/components/posts/PostHeading.js
+++ frontend/client/src/components/posts/PostHeading.js
@@ -1,9 +1,12 @@
+import getDate from "components/jobs/getDate";
 const PostHeading = (props) => {
-    return (
-        <div className="d-flex gap-1 ">
-        <div className="text-center text-light ">{props.username}</div>
-      </div>
-    )
-}
+  return (
+    <div className="d-flex gap-1 ">
+      <div className="text-center text-light ">{props.post.user.username}</div>
+      <div className="text-muted">•</div>
+      <div className="text-muted">{getDate(props.post.date)}</div>
+    </div>
+  );
+};
 
-export default PostHeading
+export default PostHeading;
