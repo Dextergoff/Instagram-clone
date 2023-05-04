ee890d561f74eadcdf5ada485c0599ff7be79491
Dextergoff <dextergoff571@gmail.com>
2023-05-04 11:58:37 -0700

    add pfp to comments

--- frontend/client/src/containers/comment/CommentSection.js
+++ frontend/client/src/containers/comment/CommentSection.js
@@ -1,6 +1,8 @@
 import { useGetCommentsQuery } from "endpoints/rtkQuery/commentEndpoints";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
+import {
+  faPlusCircle,
+} from "@fortawesome/free-solid-svg-icons";
 import React, { useState } from "react";
 import CommentInteractionBar from "../../components/comments/CommentInteractionBar";
 import CommentReplys from "./CommentReplys";
@@ -10,6 +12,7 @@ import CommentUsername from "components/comments/CommentUsername";
 import CommentBody from "components/comments/CommentBody";
 import DisplayPfp from "components/Image/DisplayPfp";
 const CommentSection = (prop) => {
+
   const [commentState, setCommentState] = useState({
     page: 1,
   });
@@ -23,22 +26,21 @@ const CommentSection = (prop) => {
   };
 
   if (getQueryLength(data) > 0)
-    // return if data is not empty
+  // return if data is not empty
     return (
       <div className="comments-container ">
         {data?.data.map((comment) => (
           <div className="" key={comment.pk}>
             <div className="d-flex gap-1">
-              <DisplayPfp
-                style={{ width: "2rem", height: "2rem", borderRadius: "100%" }}
-                pfp={process.env.REACT_APP_API_URL + comment.user.pfp}
-              />
+              <DisplayPfp pfp={process.env.REACT_APP_API_URL + comment.user.pfp}  style={{ width: "2rem", height: "2rem", borderRadius: "100%" }}  />
               <CommentUsername data={comment.user} />
               <CommentBody data={comment.body} />
             </div>
             <div className="d-flex gap-2">
-              <CommentInteractionBar comment={comment} page={page} />
-              <CreateComment comment={comment} page={page} hideform={true} />
+              <CommentInteractionBar
+              comment ={comment} page = {page}
+            />
+            <CreateComment comment = {comment} page = {page} hideform={true} />
             </div>
             <CommentReplys replysfor={comment} />
           </div>
