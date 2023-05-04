4d33d362d035f69c81039347d05762b296ed57e2
Dextergoff <dextergoff571@gmail.com>
2023-05-04 12:42:53 -0700

    rename to LikeComment

---
+++ frontend/client/src/components/comments/LikeComment.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect } from "react";
+import { useLikeCommentMutation } from "endpoints/rtkQuery/commentEndpoints";
+import updateLikes from "../../containers/comment/in_interactionbar/updateLikes";
+const LikeComment = (props) => {
+  const [interactionState, setInteractionSate] = useState({
+    display: false,
+  });
+  const { display } = interactionState;
+
+  const { userobj } = useSelector((state) => state.user);
+
+  const [likeComment, result] = useLikeCommentMutation();
+
+  const dispatch = useDispatch();
+  // const replyingto = props.data.replyingto ? props.data.replyingto : null
+  const page = props.page;
+  const comment = props.comment;
+  const pk = comment.pk;
+
+  const user = userobj?.pk;
+
+  useEffect(() => {
+    result.status === "fulfilled" &&
+      updateLikes({ dispatch, result, pk, page });
+    console.log(result);
+  }, [result, pk, page, dispatch]);
+  return (
+    <>
+      <div className="d-flex gap-2">
+        <div
+          onClick={() => likeComment({ pk, user })}
+          name="like"
+          id="like"
+          className={
+            comment.likes.includes(userobj?.pk) ? "text-danger" : "text-light"
+          }
+        >
+          <FontAwesomeIcon size="sm" icon="fa-regular fa-heart" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LikeComment;
