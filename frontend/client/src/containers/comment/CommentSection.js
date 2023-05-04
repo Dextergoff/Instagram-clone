84726b9870f2311b6e4c9004eb7d7353352943d8
Dextergoff <dextergoff571@gmail.com>
2023-05-04 12:44:24 -0700

    format (prettier) and and LikeComment

--- frontend/client/src/containers/comment/CommentSection.js
+++ frontend/client/src/containers/comment/CommentSection.js
@@ -1,10 +1,8 @@
 import { useGetCommentsQuery } from "endpoints/rtkQuery/commentEndpoints";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faPlusCircle,
-} from "@fortawesome/free-solid-svg-icons";
+import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
 import React, { useState } from "react";
-import CommentInteractionBar from "../../components/comments/CommentInteractionBar";
+import LikeComment from "../../components/comments/LikeComment";
 import CommentReplys from "./CommentReplys";
 import getQueryLength from "components/jobs/getQueryLength";
 import CreateComment from "./CreateComment";
@@ -12,7 +10,6 @@ import CommentUsername from "components/comments/CommentUsername";
 import CommentBody from "components/comments/CommentBody";
 import DisplayPfp from "components/Image/DisplayPfp";
 const CommentSection = (prop) => {
-
   const [commentState, setCommentState] = useState({
     page: 1,
   });
@@ -26,22 +23,21 @@ const CommentSection = (prop) => {
   };
 
   if (getQueryLength(data) > 0)
-  // return if data is not empty
+    // return if data is not empty
     return (
       <div className="comments-container ">
         {data?.data.map((comment) => (
           <div className="" key={comment.pk}>
             <div className="d-flex gap-1">
-              <DisplayPfp pfp={process.env.REACT_APP_API_URL + comment.user.pfp}  style={{ width: "2rem", height: "2rem", borderRadius: "100%" }}  />
+              <DisplayPfp
+                pfp={process.env.REACT_APP_API_URL + comment.user.pfp}
+                style={{ width: "2rem", height: "2rem", borderRadius: "100%" }}
+              />
               <CommentUsername data={comment.user} />
               <CommentBody data={comment.body} />
+              <LikeComment comment={comment} page={page} />
             </div>
-            <div className="d-flex gap-2">
-              <CommentInteractionBar
-              comment ={comment} page = {page}
-            />
-            <CreateComment comment = {comment} page = {page} hideform={true} />
-            </div>
+            <CreateComment comment={comment} page={page} hideform={true} />
             <CommentReplys replysfor={comment} />
           </div>
         ))}
