19742389bd1ab06e9cb66e6b4b7bcd92f03920d3
Dextergoff <dextergoff571@gmail.com>
2023-05-04 11:34:31 -0700

    add pfp styling

--- frontend/client/src/components/posts/PostHeading.js
+++ frontend/client/src/components/posts/PostHeading.js
@@ -3,7 +3,10 @@ import DisplayPfp from "components/Image/DisplayPfp";
 const PostHeading = (props) => {
   return (
     <div className="d-flex align-items-center gap-1 ">
-      <DisplayPfp pfp={process.env.REACT_APP_API_URL + props.post.user.pfp} />
+      <DisplayPfp
+        style={{ width: "2rem", height: "2rem", borderRadius: "100%" }}
+        pfp={process.env.REACT_APP_API_URL + props.post.user.pfp}
+      />
       <div className="text-center text-light ">{props.post.user.username}</div>
       <div className="text-muted">•</div>
       <div className="text-muted">{getDate(props.post.date)}</div>
