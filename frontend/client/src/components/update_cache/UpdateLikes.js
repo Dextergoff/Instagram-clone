9243def95c8ae11cd33948a39871002f0668c5e7
Dextergoff <dextergoff571@gmail.com>
2023-05-08 18:24:53 -0700

    format("prettier")

--- frontend/client/src/components/update_cache/UpdateLikes.js
+++ frontend/client/src/components/update_cache/UpdateLikes.js
@@ -1,25 +1,26 @@
 import { splitApi } from "endpoints/rtkQuery/splitApi";
-const UpdateLikes = ({queryName, addArgument, updateCacheArgument, result, dispatch}) => {
-
-    return (
-        dispatch(
-            splitApi.util.updateQueryData(
-                queryName,
-                addArgument
-                    ? { updateCacheArgument, addArgument }
-                    : updateCacheArgument,
-                (response) => {
-                    if (response.nested_data) {
-                        response = response.nested_data.data.find(
-                            (item) => item.pk === result.data.pk
-                        );
-                    }
-                    response.likes = result.data.likes;
-                    response.likecount = result.data.likecount;
-                }
-            )
-        )
+const UpdateLikes = ({
+  queryName,
+  addArgument,
+  updateCacheArgument,
+  result,
+  dispatch,
+}) => {
+  return dispatch(
+    splitApi.util.updateQueryData(
+      queryName,
+      addArgument ? { updateCacheArgument, addArgument } : updateCacheArgument,
+      (response) => {
+        if (response.nested_data) {
+          response = response.nested_data.data.find(
+            (item) => item.pk === result.data.pk
+          );
+        }
+        response.likes = result.data.likes;
+        response.likecount = result.data.likecount;
+      }
     )
-}
+  );
+};
 
-export default UpdateLikes
+export default UpdateLikes;
