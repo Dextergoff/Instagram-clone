c5bcb03b75fa859c98174dcb3ed2699772ecdb5c
Dextergoff <dextergoff571@gmail.com>
2023-05-09 18:41:09 -0700

    update according to new variable names

--- frontend/client/src/components/rtkQuery/handleNewComments.js
+++ frontend/client/src/components/rtkQuery/handleNewComments.js
@@ -1,23 +1,24 @@
-const handleNewComments = ({newItems, currentCache}) => {
-    const isnewpost = newItems.parent !== currentCache.parent
-    const isoldpost = currentCache.page !== newItems.page
-
-    const ReplaceComments = () => {
-      for (let i in currentCache) {
-        currentCache[i] = newItems[i];
-      }
-    };
-
-    const PushComments = () => {
-      currentCache.data.push(...newItems.comments);
-      currentCache.end_of_data = newItems.end_of_data;
-    };
-
-    if (isnewpost) {
-      ReplaceComments();
-    } else if (isoldpost) {
-      PushComments();
+const handleNewComments = ({ newItems, currentCache, args }) => {
+  const ReplaceComments = () => {
+    for (let i in currentCache) {
+      currentCache[i] = newItems[i];
     }
-}
+  };
 
-export { handleNewComments }
+  const PushComments = () => {
+    currentCache.data.push(...newItems.data);
+    currentCache.end_of_data = newItems.end_of_data;
+  };
+
+  if (newItems.post !== currentCache.post) {
+    ReplaceComments();
+  }
+
+  if (newItems.page !== currentCache.page) {
+    PushComments();
+  }
+
+  // using this current logic to push replys is duplicating comments
+};
+
+export { handleNewComments };
