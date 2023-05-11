fee0d1fb60874fab44d8e17353f99aa33c74186b
Dextergoff <dextergoff571@gmail.com>
2023-05-11 13:22:07 -0700

    deleted

--- frontend/client/src/components/Image/GalleryPostImage.js
+++
@@ -1,9 +0,0 @@
-const GalleryPostImage = (props) => {
-    return (
-      <div>
-         <img alt="" src={process.env.REACT_APP_API_URL + props.image} />
-      </div>
-    );
-  };
-  export default GalleryPostImage;
-  
