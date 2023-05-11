eac336908f27a678f90118ba6a05cc85620f92fb
Dextergoff <dextergoff571@gmail.com>
2023-05-10 17:02:50 -0700

    add refetchonfocus

--- frontend/client/src/endpoints/rtkQuery/splitApi.js
+++ frontend/client/src/endpoints/rtkQuery/splitApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const splitApi = createApi({
   reducerPath: "api",
-
+  refetchOnFocus: true,
   baseQuery: fetchBaseQuery({
     baseUrl: "http://127.0.0.1:8000/",
     credentials: "same-origin",
