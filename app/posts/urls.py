e9c8e8b247f468336520bfb75c9ddb5bf44cb3ce
Dextergoff <dextergoff571@gmail.com>
2023-05-11 19:36:47 -0700

    support post request on page endpoint

--- app/posts/urls.py
+++ app/posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import *
-app_name="posts"
-urlpatterns = [ 
+app_name = "posts"
+urlpatterns = [
     path('post/<pk>', PostsView.as_view({'get': 'main'}), ),
-    path('page/<int:page>', PageView.as_view({'get': 'main'}), ),
+    path('page/<int:page>/',
+         PageView.as_view({'get': 'main', 'post': 'main'}), ),
     path('likepost/', LikePost.as_view({'post': 'like'}), ),
     path('createpost/', CreatePostView.as_view(), ),
 ]
