528d882e38e4c19cf7c04ea4a7f35b836b95f607
Dextergoff <dextergoff571@gmail.com>
2023-05-13 14:22:09 -0700

    update url names

--- app/users/urls.py
+++ app/users/urls.py
@@ -4,8 +4,8 @@ app_name="user"
 urlpatterns = [
     path('register', RegisterView.as_view()),
     path('me', RetriveUserView.as_view()),
-    path('resetpasswordsendmail', ResetPasswordSendMail.as_view()),
+    path('forgot', ResetPasswordSendMail.as_view()),
     path('resetpassword', ResetPassword.as_view()),
-    path('resetpasswordverify', ResetPasswordVerify.as_view()),
+    path('verify_password', ResetPasswordVerify.as_view()),
     
 ]
