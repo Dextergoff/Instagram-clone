d31fb97e078aa8926ed648e3abf13e0eca65e322
Dextergoff <dextergoff571@gmail.com>
2023-05-13 22:57:07 -0700

    update parent of replys  bool field

--- frontend/client/src/components/update_cache/UpdateComments.js
+++ frontend/client/src/components/update_cache/UpdateComments.js
@@ -1,12 +1,20 @@
 import { splitApi } from "endpoints/rtkQuery/splitApi";
 const UpdateComments = ({ parent, dispatch, result, page, queryName }) => {
   parent = parent.pk;
-  const args = { parent, page };
-  return dispatch(
-    splitApi.util.updateQueryData(queryName, args, (data) => {
-      data.data.unshift(result.data.comment);
-    })
+  dispatch(
+    splitApi.util.updateQueryData(queryName, {parent, page}, (i) => {
+      i.data.unshift(result.data.comment);
+    }),
+ 
   );
+  if(queryName === "getReplys"){
+    dispatch(
+      splitApi.util.updateQueryData("getComments", undefined, (j) => {
+        let k = j.data.find((j) => j.pk == Number(parent))
+        k.children = k.children + 1
+      } ),
+    )
+  }
 };
 
 export default UpdateComments;
