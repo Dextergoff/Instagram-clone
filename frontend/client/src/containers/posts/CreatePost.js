fe7e39b96a7824a56e69ed7e6d2ef300c727f363
Dextergoff <dextergoff571@gmail.com>
2023-05-13 23:12:31 -0700

    completed todo

--- frontend/client/src/containers/posts/CreatePost.js
+++ frontend/client/src/containers/posts/CreatePost.js
@@ -11,7 +11,6 @@ import UserExists from "components/jobs/verification/UserExists";
 import TitleInput from "components/inputs/TitleInput";
 import "./css/imagelabel.css";
 const CreatePost = () => {
-  // TODO fix the image preview and add warning for when title is too long
   const navigate = useNavigate();
   const { userobj, loading } = useSelector((state) => state.user);
 
