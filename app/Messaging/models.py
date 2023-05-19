dbe10e7b3ee0859a8be429ea0c33ff6b82f779ab
Dextergoff <dextergoff571@gmail.com>
2023-05-19 13:33:53 -0700

    move chatroom above messages to access it with for key

--- app/Messaging/models.py
+++ app/Messaging/models.py
@@ -2,12 +2,16 @@ from django.contrib.auth import get_user_model
 from django.db import models
 User = get_user_model()
 
-class Messages(models.Model):
-    content = models.CharField(max_length=500, blank=True, null=True)
 
 class ChatRoom(models.Model):
-    sender_name = models.CharField(
-        max_length=500, blank=True, null=True)
-    receiver_name = models.CharField(
-        max_length=500, blank=True, null=True)
-    message = models.CharField(max_length=500, blank=True, null=True)
+    sender = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="sender")
+    receiver = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="receiver")
+
+    #
+
+
+class Messages(models.Model):
+    parent = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
+    content = models.CharField(max_length=500, blank=True, null=True)
