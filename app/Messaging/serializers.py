989084b01b0b18a22f29bfac3e36a30e15529905
Dextergoff <dextergoff571@gmail.com>
2023-05-19 13:34:49 -0700

    create a serializer for messages

---
+++ app/Messaging/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
+from rest_framework.decorators import api_view
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import filters
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.settings import api_settings
+from django.contrib.auth.models import update_last_login
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import get_object_or_404
+from .models import *
+from .serializers import *
+from users.serializers import *
+User = get_user_model()
+
+
+class MessageSerializer(serializers.Serializer):
+    def get_messages(self):
+        pass
+
+
+class ChatRoomSerializer(serializers.ModelSerializer):
+    messages = MessageSerializer(many=True)
+
+    class Meta:
+        model = Messages
+        fields = [
+            'pk',
+            'sender',
+            'receiver',
+            'messages',
+        ]
