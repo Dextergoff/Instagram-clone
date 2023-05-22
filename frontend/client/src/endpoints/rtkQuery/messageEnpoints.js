49d34808626dadb8efa70cab15f280f6f279341a
Dextergoff <dextergoff571@gmail.com>
2023-05-22 14:07:41 -0700

    merge new comments when new page is requested

--- frontend/client/src/endpoints/rtkQuery/messageEnpoints.js
+++ frontend/client/src/endpoints/rtkQuery/messageEnpoints.js
@@ -12,6 +12,22 @@ splitApi.injectEndpoints({
           "Content-type": "application/json",
         },
       }),
+
+      serializeQueryArgs: ({ getComments }) => {
+        return getComments;
+      },
+      merge: (currentCache, newItems, args) => {
+        currentCache.data.unshift(...newItems.data);
+        currentCache.end_of_data = newItems.end_of_data;
+        // TODO items keep being pushed if page is revisted without refresh could probaly implement same fix that is used in the comments endpoint
+      },
+      forceRefetch({ currentArg, previousArg }) {
+        return currentArg !== previousArg;
+      },
+      transformResponse: (response, arg) => {
+        response.data = [...response.data].reverse();
+        return response;
+      },
     }),
   }),
 });
