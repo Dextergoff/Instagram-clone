943ebce329e4e0a47f4d84bcaf05880b3e7b31fc
Dextergoff <dextergoff571@gmail.com>
2023-05-22 12:41:44 -0700

    add fields

--- app/Messaging/models.py
+++ app/Messaging/models.py
@@ -1,17 +1,18 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.timezone import now
+
 User = get_user_model()
 
 
-class ChatRoom(models.Model):
-    sender = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="sender")
-    receiver = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="receiver")
+# class ChatRoom(models.Model):
+#     sender = models.ForeignKey(
+#         User, on_delete=models.CASCADE, related_name="sender")
+#     receiver = models.ForeignKey(
+#         User, on_delete=models.CASCADE, related_name="receiver")
 
-    #
-
-
-class Messages(models.Model):
-    parent = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
-    content = models.CharField(max_length=500, blank=True, null=True)
+class Message(models.Model):
+    sender = models.CharField(max_length=500, blank=True, null=True)
+    message = models.CharField(max_length=500, blank=True, null=True)
+    room_name = models.CharField(max_length=500, blank=True, null=True)
+    date = models.DateTimeField(default=now)
