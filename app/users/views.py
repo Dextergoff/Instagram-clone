7425eea39db895022d2ea9cb7222331736cb3e57
Dextergoff <dextergoff571@gmail.com>
2023-05-22 12:39:55 -0700

    ws

--- app/users/views.py
+++ app/users/views.py
@@ -8,19 +8,21 @@ from rest_framework.response import Response
 import jwt
 import uuid
 from center import settings
-User=get_user_model()
+User = get_user_model()
+
 
 class Mail():
-    def send_email(data,uid):
-        send_mail( 
+    def send_email(data, uid):
+        send_mail(
             'reset password',
             'http://localhost:3000/resetpassword?uid='+uid,
             'dextergoff571@gmail.com',
             [data['email']]
-        ) 
+        )
+
 
 class CreateResetKey():
-   def set_uid(user):
+    def set_uid(user):
         uid = str(uuid.uuid4())
         user.resetkey = uid
         user.save()
@@ -32,22 +34,24 @@ class Tokens():
     def get_token_data(data):
         try:
             token = data['token']
-            payload = jwt.decode(token, key=settings.SECRET_KEY, algorithms=['HS256'])
+            payload = jwt.decode(
+                token, key=settings.SECRET_KEY, algorithms=['HS256'])
             return payload
         except Exception as e:
             raise e
-    
+
     def create_token(data):
         try:
             user = User.objects.get(email=data['email'])
-            token =  AccessToken.for_user(user)
+            token = AccessToken.for_user(user)
             uid = CreateResetKey.set_uid(user)
         except Exception as e:
             raise exceptions.BadRequest(('could not create token'))
-        return {"token":str(token), "uid":uid}
+        return {"token": str(token), "uid": uid}
 
     def get_token_from_response(response):
         return response[0][1]
+
 
 class Validation():
     def validate_email(data):
@@ -55,7 +59,8 @@ class Validation():
             user = User.objects.get(email=data)
             return user
         except exceptions.ValidationError:
-            raise exceptions.ValidationError(('Couldnt find account with this email'))
+            raise exceptions.ValidationError(
+                ('Couldnt find account with this email'))
 
     def validate_password(data):
         newpassword = data
@@ -65,5 +70,5 @@ class Validation():
         except exceptions.ValidationError as e:
             serializer_errors = serializers.as_serializer_error(e)
             raise exceptions.ValidationError(
-                {'errors':serializer_errors['non_field_errors']}
+                {'errors': serializer_errors['non_field_errors']}
             )
