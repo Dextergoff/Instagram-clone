69cebe229821cc22f2f26b4d7229b601ce68d0c8
Dextergoff <dextergoff571@gmail.com>
2023-05-22 14:05:51 -0700

    display user info on old messages

--- frontend/client/src/containers/messages/OldMessages.js
+++ frontend/client/src/containers/messages/OldMessages.js
@@ -1,36 +1,47 @@
 import UserDetails from "components/posts/UserDetails";
 import { useGetMessagesQuery } from "endpoints/rtkQuery/messageEnpoints";
+import LoadContent from "components/posts/LoadContent";
 import { useState } from "react";
+import DisplayPfp from "components/Image/DisplayPfp";
 const OldMessages = ({ calc_room }) => {
-  const [msgState, setMsgState] = useState({
+  const [state, setState] = useState({
     page: 1,
   });
-  const { page } = msgState;
+  const { page } = state;
   const { data = [] } = useGetMessagesQuery({ calc_room, page });
-  if (data.data)
+  if (data.data) {
     return (
       <>
         {/* TODO set room name to target user and use a user id or token to seperate chats */}
-        <div
-          style={{
-            height: 500,
-            maxHeight: 500,
-            overflow: "auto",
-            boxShadow: "none",
-          }}
-        >
-          {data.data.map((message) => (
-            <div key={message} className="d-flex justify-content-end">
-              <div
-                style={{ width: "fit-content" }}
-                className="p-2 bg-primary rounded-pill pl-2"
-              >
-                <div className="">{message.message}</div>
+
+        <LoadContent data={data} states={{ state, setState }} />
+        {data.data.map((message) => (
+          <div key={message.pk} className="d-flex justify-content-start">
+            <div
+              style={{ width: "fit-content" }}
+              className="p-2 text-light pl-2"
+            >
+              <div className="d-flex align-items-center gap-1 ">
+                <DisplayPfp
+                  style={{
+                    width: "2rem",
+                    height: "2rem",
+                    borderRadius: "100%",
+                  }}
+                  pfp={process.env.REACT_APP_API_URL + message.user.pfp}
+                />
+                <div>
+                  <div className="text-center text-light ">
+                    {message.user.username}
+                  </div>
+                  <div className="">{message.message}</div>
+                </div>
               </div>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </>
     );
+  }
 };
 export default OldMessages;
