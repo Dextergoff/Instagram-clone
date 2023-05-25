b138d81a8f3031bcd805bea6e49501d07a21bc8e
Dextergoff <dextergoff571@gmail.com>
2023-05-24 18:57:50 -0700

    remove state and servedData from props

--- frontend/client/src/containers/messages/HandleServedData.js
+++ frontend/client/src/containers/messages/HandleServedData.js
@@ -1,12 +1,5 @@
 import { splitApi } from "endpoints/rtkQuery/splitApi";
-const HandleServedData = ({
-  servedData,
-  state,
-  calc_room,
-  dispatch,
-  message,
-}) => {
-  const { page } = state;
+const HandleServedData = ({ page, calc_room, dispatch, message }) => {
   const jsonMessage = JSON.parse(message.data);
   dispatch(
     splitApi.util.updateQueryData("getMessages", { calc_room, page }, (i) => {
