78e1381184aefe45c9ec87500492a7365b00cf7d
Dextergoff <dextergoff571@gmail.com>
2023-05-24 18:56:58 -0700

    add correct dependacies for the useffect

--- frontend/client/src/containers/messages/MessageWS.js
+++ frontend/client/src/containers/messages/MessageWS.js
@@ -11,60 +11,51 @@ import { useDispatch } from "react-redux";
 import Messages from "./Messages";
 import HandleServedData from "./HandleServedData";
 const MessageWS = () => {
-  // TODO but this in a try catch nad navigate to login on catch
-
   const { userobj } = useSelector((state) => state.user);
   const dispatch = useDispatch();
   const location = useLocation();
 
   const { target_user } = location.state;
 
-  const [state, setState] = useState({
-    page: 1,
-    connected: false,
-  });
-
   const calc_room = target_user?.pk * 3 + userobj?.pk * 3;
 
+  const [state, setState] = useState({
+    page: 1,
+  });
+  const { page } = state;
+  // TODO clean up html, increase amount of messages per page, make the sidebar for users others chats display what user your talking too on both sides of the chat
   const client = new w3cwebsocket("ws://127.0.0.1:8000/ws/" + calc_room);
+  useEffect(() => {
+    client.onmessage = (message) => {
+      HandleServedData({
+        page,
+        message,
+        dispatch,
+        calc_room,
+      });
+      console.log(message);
+    };
+  }, [dispatch, calc_room]);
 
-  client.onmessage = (message) => {
-    HandleServedData({
-      message,
-      state,
-      dispatch,
-      calc_room,
-    });
-  };
-  // websocket sending message three times?!
-
-  // client.onmessage = function (e) {
-  //   if (typeof e.data === "string") {
-  //     console.log("Received: '" + e.data + "'");
-  //   }
-  // };
-
-  if (userobj) {
-    return (
-      <Layout>
-        <div className="d-flex flex-row">
-          <div>
-            <Sidebar
-              style={{
-                maxHeight: "95vh",
-                width: "40vh",
-              }}
-            />
-          </div>
-          <div>
-            <UserDetails user={target_user} />
-            <Messages calc_room={calc_room} />
-            <SendMessage userobj={userobj} client={client} />
-          </div>
+  return (
+    <Layout>
+      <div className="d-flex flex-row">
+        <div>
+          <Sidebar
+            style={{
+              maxHeight: "95vh",
+              width: "40vh",
+            }}
+          />
         </div>
-      </Layout>
-    );
-  }
+        <div>
+          <UserDetails user={target_user} />
+          <Messages states={{ state, setState }} calc_room={calc_room} />
+          <SendMessage client={client} />
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default MessageWS;
