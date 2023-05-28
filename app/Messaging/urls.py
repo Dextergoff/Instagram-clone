c27568d97370b72c694291d916e68816be99aeca
Dextergoff <dextergoff571@gmail.com>
2023-05-28 12:56:16 -0700

    create enpoint for getting chatrooms

--- app/Messaging/urls.py
+++ app/Messaging/urls.py
@@ -5,5 +5,6 @@ app_name = "Messaging"
 urlpatterns = [
     path('messages/<room_name>/<int:page>',
          GetMessages.as_view({'get': 'main'}), ),
-
+    path('chatroom/<user>',
+         GetChatRoom.as_view({'get': 'main'}), ),
 ]
