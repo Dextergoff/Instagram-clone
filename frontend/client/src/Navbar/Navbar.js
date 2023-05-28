7a1d48463e113a4958cc9780294376f8ed623156
Dextergoff <dextergoff571@gmail.com>
2023-05-28 14:51:36 -0700

    set chatroom url to /dm and messaging url to /dm/u

--- frontend/client/src/Navbar/Navbar.js
+++ frontend/client/src/Navbar/Navbar.js
@@ -60,7 +60,7 @@ const Navbar = () => {
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link mx-5" to="/">
+                <NavLink className="nav-link mx-5" to="/dm">
                   <FontAwesomeIcon
                     className="text-light"
                     icon="fa-regular fa-paper-plane"
