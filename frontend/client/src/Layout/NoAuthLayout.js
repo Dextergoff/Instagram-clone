53ef13f1e16dfb521fcc11a413b7e60ee3294dac
Dextergoff <dextergoff571@gmail.com>
2023-05-30 12:45:02 -0700

    add a conditional loading screen

--- frontend/client/src/Layout/NoAuthLayout.js
+++ frontend/client/src/Layout/NoAuthLayout.js
@@ -1,10 +1,30 @@
+import BootstrapSpinner from "components/bootstrap/BootstrapSpinner";
+import { useSelector } from "react-redux";
 const NoAuthLayout = (props) => {
-  return (
-    <>
-      <style>{"body { background-color: #000; }"}</style>
-      <div className="">{props.children}</div>
-    </>
-  );
+  const { loading } = useSelector((state) => state.user);
+  if (!loading) {
+    return (
+      <>
+        <style>{"body { background-color: #000; }"}</style>
+        <div className="">{props.children}</div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <style>{"body { background-color: #000; }"}</style>
+        <div
+          style={{
+            position: "fixed" /* or absolute */,
+            top: "50%",
+            left: "50%",
+          }}
+        >
+          <BootstrapSpinner />
+        </div>
+      </>
+    );
+  }
 };
 
 export default NoAuthLayout;
