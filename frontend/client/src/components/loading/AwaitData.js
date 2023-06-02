21f43c06c5379c772340870b84d184ec9d3177da
Dextergoff <dextergoff571@gmail.com>
2023-06-02 14:22:36 -0700

    create a loading screen for when data is not loaded

---
+++ frontend/client/src/components/loading/AwaitData.js
@@ -0,0 +1,32 @@
+import Navbar from "Navbar/Navbar";
+import { Helmet } from "react-helmet";
+import { useSelector } from "react-redux";
+import BootstrapSpinner from "components/bootstrap/BootstrapSpinner";
+const AwaitData = (props) => {
+  const { isAuthenticated } = useSelector((state) => state.user);
+  if (props.data) {
+    return (
+      <>
+        <style>{"body { background-color: #000; }"}</style>
+        <div className="">{props.children}</div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <style>{"body { background-color: #000; }"}</style>
+        <div
+          style={{
+            position: "fixed" /* or absolute */,
+            top: "50%",
+            left: "50%",
+          }}
+        >
+          <BootstrapSpinner />
+        </div>
+      </>
+    );
+  }
+};
+
+export default AwaitData;
