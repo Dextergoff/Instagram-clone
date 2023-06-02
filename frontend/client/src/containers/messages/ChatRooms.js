2ab51f253322786c5e6ae6b49ad3f79e295b4375
Dextergoff <dextergoff571@gmail.com>
2023-06-02 14:23:32 -0700

    create a landing page for chatrooms

--- frontend/client/src/containers/messages/ChatRooms.js
+++ frontend/client/src/containers/messages/ChatRooms.js
@@ -4,46 +4,114 @@ import { useGetChatRoomQuery } from "endpoints/rtkQuery/messageEnpoints";
 import UserDetails from "containers/posts/UserDetails";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import MessageWS from "./MessageWS";
+import AwaitData from "components/loading/AwaitData";
 const ChatRooms = () => {
+  const location = useLocation();
   const { userobj } = useSelector((state) => state.user);
+
   const [state, setState] = useState({
     skip: true,
+    target_user: null,
   });
-  const { skip } = state;
+  const { target_user, skip } = state;
+
   const { data } = useGetChatRoomQuery(userobj?.pk, { skip: skip });
+
+  const handleClick = (target_user) => {
+    setState({ ...state, target_user: target_user });
+  };
+
   useEffect(() => {
     if (userobj?.pk) {
-      setState({ skip: false });
+      setState({ ...state, skip: false });
     }
   }, [userobj]);
-  // TODO make this look instagrams intial inbox page
+
+  useEffect(() => {
+    try {
+      setState({ ...state, target_user: location.state.target_user });
+    } catch (error) {}
+  }, [location.state]);
+
   return (
-    <Layout>
-      <div>
-        {data?.data.map((room) => (
-          <div className="p-3">
-            <Link
-              className="comments-link text-muted fw-light text-decoration-none"
-              to={`/dm/u`}
-              state={{
-                target_user: room.receiver,
+    <AwaitData data={data}>
+      <Layout data={data}>
+        <div className="d-flex flex-row">
+          <div
+            style={{
+              height: "100vh",
+              flexGrow: 1,
+              borderStyle: "solid",
+              borderWidth: "1px",
+            }}
+          >
+            <div
+              style={{
+                fontSize: "1rem",
               }}
+              className="p-2 text-light"
             >
-              <div className="">
-                <UserDetails
-                  user={room.receiver}
-                  style={{
-                    width: "3rem",
-                    height: "3rem",
-                    borderRadius: "100%",
+              {userobj?.username}
+            </div>
+            <div
+              style={{
+                fontSize: "0.8rem",
+              }}
+              className="p-2 text-light"
+            >
+              Messages
+            </div>
+            {data?.data.map((room) => (
+              <div className="p-2">
+                <button
+                  onClick={() => handleClick(room.receiver)}
+                  className="comments-link text-muted bg-black border-0 fw-light text-decoration-none"
+                  to={`/dm/u`}
+                  state={{
+                    target_user: room.receiver,
                   }}
-                />
+                >
+                  <div
+                    style={{
+                      width: "13vw",
+                    }}
+                  >
+                    <UserDetails
+                      user={room.receiver}
+                      style={{
+                        width: "2.5rem",
+                        height: "2.5rem",
+                        borderRadius: "100%",
+                      }}
+                    />
+                    <div
+                      style={{
+                        fontSize: "0.8rem",
+                      }}
+                      className="d-flex align-self-start"
+                    >
+                      last message test • 1h ago
+                    </div>
+                    {/* TODO implement last messages and truncate to "..." after a few chars */}
+                  </div>
+                </button>
               </div>
-            </Link>
+            ))}
           </div>
-        ))}
-      </div>
-    </Layout>
+          <div
+            style={{
+              height: "100vh",
+              width: "80vw",
+              flexGrow: 2,
+            }}
+          >
+            <MessageWS target_user={target_user} />
+          </div>
+        </div>
+      </Layout>
+    </AwaitData>
   );
 };
 export default ChatRooms;
