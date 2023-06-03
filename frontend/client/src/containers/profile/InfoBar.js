8969f8a7f73548c1c3234077bbe794e07ec9064c
Dextergoff <dextergoff571@gmail.com>
2023-06-03 14:49:40 -0700

    remove post count

--- frontend/client/src/containers/profile/InfoBar.js
+++ frontend/client/src/containers/profile/InfoBar.js
@@ -1,17 +1,13 @@
-const InfoBar = ({data}) => {
-return(
-    <div className="d-flex gap-5 mt-4 justify-content-center">
-        <div className="h6 align-self-center text-light fw-light">
-          {data.post_count} posts
-        </div>
-        <div className="h6 align-self-center text-light fw-light">
-          0 followers
-        </div>
-        <div className="h6 align-self-center text-light fw-light">
-          0 following
-        </div>
+const InfoBar = ({ data }) => {
+  return (
+    <div className="d-flex gap-5 mt-4 mb-3 justify-content-center">
+      <div className="h6 align-self-center text-light fw-light">
+        0 followers
       </div>
-)
-
-}
-export default InfoBar
+      <div className="h6 align-self-center text-light fw-light">
+        0 following
+      </div>
+    </div>
+  );
+};
+export default InfoBar;
