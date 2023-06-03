adfc92292db735b1a37ff74171986cdd9ef6b3ca
Dextergoff <dextergoff571@gmail.com>
2023-06-03 14:47:09 -0700

    ws

--- frontend/client/src/containers/comment/UpdateComments.js
+++ frontend/client/src/containers/comment/UpdateComments.js
@@ -2,18 +2,17 @@ import { splitApi } from "endpoints/rtkQuery/splitApi";
 const UpdateComments = ({ parent, dispatch, result, page, queryName }) => {
   parent = parent.pk;
   dispatch(
-    splitApi.util.updateQueryData(queryName, {parent, page}, (i) => {
+    splitApi.util.updateQueryData(queryName, { parent, page }, (i) => {
       i.data.unshift(result.data.comment);
-    }),
- 
+    })
   );
-  if(queryName === "getReplys"){
+  if (queryName === "getReplys") {
     dispatch(
       splitApi.util.updateQueryData("getComments", undefined, (j) => {
-        let p = j.data.find((j) => j.pk == Number(parent))
-        p.has_replys = true
-      } ),
-    )
+        let p = j.data.find((j) => j.pk == Number(parent));
+        p.has_replys = true;
+      })
+    );
   }
 };
 
