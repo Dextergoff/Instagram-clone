d05102e616e2bfa6f8231d9b8c4f8f49c7f92636
Dextergoff <dextergoff571@gmail.com>
2023-06-04 14:15:34 -0700

    replace profile data if use changes

--- frontend/client/src/endpoints/rtkQuery/mergeNewItems.js
+++ frontend/client/src/endpoints/rtkQuery/mergeNewItems.js
@@ -1,9 +1,10 @@
-const mergeNewItems = ({ currentCache, newItems }) => {
-  const includespage = currentCache.nested_data.page.some((page) =>
-    newItems.nested_data.page.includes(page)
-  );
-
-  if (!includespage) {
+const mergeNewItems = ({ currentCache, newItems, args }) => {
+  console.log(newItems.nested_data.page);
+  if (currentCache.nested_data.user.pk !== newItems.nested_data.user.pk) {
+    currentCache.nested_data = newItems.nested_data;
+    currentCache.nested_data.page = newItems.nested_data.page;
+    currentCache.end_of_data = newItems.end_of_data;
+  } else {
     currentCache.nested_data.data.push(...newItems.nested_data.data);
     currentCache.nested_data.page.push(...newItems.nested_data.page);
     currentCache.end_of_data = newItems.end_of_data;
