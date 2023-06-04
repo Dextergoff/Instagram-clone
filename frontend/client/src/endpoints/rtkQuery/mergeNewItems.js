920d6b17e417aae151e935fc91fee2125e5aef28
Dextergoff <dextergoff571@gmail.com>
2023-06-04 16:52:26 -0700

    ws

--- frontend/client/src/endpoints/rtkQuery/mergeNewItems.js
+++ frontend/client/src/endpoints/rtkQuery/mergeNewItems.js
@@ -1,10 +1,9 @@
-const mergeNewItems = ({ currentCache, newItems, args }) => {
-  console.log(newItems.nested_data.page);
-  if (currentCache.nested_data.user.pk !== newItems.nested_data.user.pk) {
-    currentCache.nested_data = newItems.nested_data;
-    currentCache.nested_data.page = newItems.nested_data.page;
-    currentCache.end_of_data = newItems.end_of_data;
-  } else {
+const mergeNewItems = ({ currentCache, newItems }) => {
+  const includespage = currentCache.nested_data.page.some((page) =>
+    newItems.nested_data.page.includes(page)
+  );
+
+  if (!includespage) {
     currentCache.nested_data.data.push(...newItems.nested_data.data);
     currentCache.nested_data.page.push(...newItems.nested_data.page);
     currentCache.end_of_data = newItems.end_of_data;
