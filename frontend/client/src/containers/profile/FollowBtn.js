f819a8ba35eb0e6a70c9bad78bdd877bbbb3c5f5
Dextergoff <dextergoff571@gmail.com>
2023-06-05 18:42:35 -0700

    seperate follow button into a component

---
+++ frontend/client/src/containers/profile/FollowBtn.js
@@ -0,0 +1,44 @@
+import { useManageFollowersMutation } from "endpoints/rtkQuery/profileEndpoints";
+import MessageBtn from "components/buttons/MessageBtn";
+const FollowBtn = ({ is_self, is_following, requested_user, userobj }) => {
+  const [manage_followers] = useManageFollowersMutation();
+  const requested_user_pk = requested_user.pk;
+  const pk = userobj.pk;
+
+  if (!is_self) {
+    return (
+      <>
+        {!is_following ? (
+          <div className="d-flex flex-row gap-3">
+            <div
+              onClick={() => manage_followers({ pk, requested_user_pk })}
+              className="align-self-center btn btn-sm btn-primary"
+            >
+              Follow
+            </div>
+
+            <div className="align-self-center">
+              <MessageBtn target={requested_user} size="sm" />
+            </div>
+          </div>
+        ) : (
+          <div className="d-flex flex-row gap-3">
+            <div
+              onClick={() => manage_followers({ pk, requested_user_pk })}
+              className="align-self-center btn btn-sm btn-dark"
+            >
+              Unfollow
+            </div>
+
+            <div className="align-self-center">
+              <MessageBtn target={requested_user} size="sm" />
+            </div>
+          </div>
+        )}
+      </>
+    );
+  } else {
+    <></>;
+  }
+};
+export default FollowBtn;
