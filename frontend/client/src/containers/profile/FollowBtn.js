dd6f2c1ccef691b10bcf7d8f64c2cd34b00740c2
Dextergoff <dextergoff571@gmail.com>
2023-06-06 16:00:23 -0700

    create router links for follower popup

--- frontend/client/src/containers/profile/FollowBtn.js
+++ frontend/client/src/containers/profile/FollowBtn.js
@@ -1,15 +1,16 @@
 import { useManageFollowersMutation } from "endpoints/rtkQuery/profileEndpoints";
 import MessageBtn from "components/buttons/MessageBtn";
-const FollowBtn = ({ is_self, is_following, requested_user, userobj }) => {
+const FollowBtn = ({ is_following, requested_user, userobj }) => {
   const [manage_followers] = useManageFollowersMutation();
   const requested_user_pk = requested_user.pk;
   const pk = userobj.pk;
+  const is_self = Boolean(userobj?.pk == requested_user.pk);
 
   if (!is_self) {
     return (
       <>
         {!is_following ? (
-          <div className="d-flex flex-row gap-3">
+          <div className="d-flex flex-row gap-2">
             <div
               onClick={() => manage_followers({ pk, requested_user_pk })}
               className="align-self-center btn btn-sm btn-primary"
@@ -27,7 +28,7 @@ const FollowBtn = ({ is_self, is_following, requested_user, userobj }) => {
               onClick={() => manage_followers({ pk, requested_user_pk })}
               className="align-self-center btn btn-sm btn-dark"
             >
-              Unfollow
+              Following
             </div>
 
             <div className="align-self-center">
