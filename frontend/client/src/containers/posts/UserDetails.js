9e1710b887b7fa683d2422fa5fbb7feb20d6deb8
Dextergoff <dextergoff571@gmail.com>
2023-06-06 15:59:46 -0700

    add a optional description

--- frontend/client/src/containers/posts/UserDetails.js
+++ frontend/client/src/containers/posts/UserDetails.js
@@ -3,6 +3,8 @@ import DisplayPfp from "components/Image/DisplayPfp";
 import { Navigate } from "react-router-dom";
 const UserDetails = (props) => {
   const link_disabled = props.disable_link || false;
+  const display_desc = props.display_desc || false;
+
   const style = props.style || {
     width: "2rem",
     height: "2rem",
@@ -17,19 +19,37 @@ const UserDetails = (props) => {
             pfp={process.env.REACT_APP_API_URL + props.user.pfp}
           />
           <div className="text-center text-light ">{props.user.username}</div>
+          {display_desc ? (
+            <div className="text-center text-light ">
+              {props.user.description}
+            </div>
+          ) : (
+            <></>
+          )}
         </div>
       </>
     );
   } else {
     return (
       <>
-        <a href={`u/${props.user.pk}`} style={{ textDecoration: "none" }}>
+        <a href={`/u/${props.user.pk}`} style={{ textDecoration: "none" }}>
           <div className="d-flex align-items-center gap-2 ">
             <DisplayPfp
               style={style}
               pfp={process.env.REACT_APP_API_URL + props.user.pfp}
             />
-            <div className="text-center text-light ">{props.user.username}</div>
+            <div>
+              <div className="text-center text-light ">
+                {props.user.username}
+              </div>
+              {display_desc ? (
+                <div style={{ fontSize: ".7rem" }} className=" text-light ">
+                  {props.user.description}
+                </div>
+              ) : (
+                <></>
+              )}
+            </div>
           </div>
         </a>
       </>
