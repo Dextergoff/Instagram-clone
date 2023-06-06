2f2385b0f085b68c81e2e4030d459b3460000532
Dextergoff <dextergoff571@gmail.com>
2023-06-06 16:00:31 -0700

    create router links for follower popup

--- frontend/client/src/containers/profile/InfoBar.js
+++ frontend/client/src/containers/profile/InfoBar.js
@@ -1,11 +1,30 @@
-const InfoBar = ({ requested_user }) => {
+import { Link } from "react-router-dom";
+const InfoBar = ({ requested_user, location }) => {
   return (
     <div className="d-flex gap-5 mt-4 mb-3 justify-content-center">
       <div className="h6 align-self-center text-light fw-light">
-        Followers {requested_user.followers_count}
+        <Link
+          className="comments-link text-light fw-light text-decoration-none"
+          to={`/followers/`}
+          state={{
+            background: location,
+            requested_user: requested_user,
+          }}
+        >
+          Followers {requested_user.followers_count}
+        </Link>
       </div>
       <div className="h6 align-self-center text-light fw-light">
-        Following {requested_user.following_count}
+        <Link
+          className="comments-link text-light fw-light text-decoration-none"
+          to={`/following/`}
+          state={{
+            background: location,
+            requested_user: requested_user,
+          }}
+        >
+          Following {requested_user.following_count}
+        </Link>
       </div>
     </div>
   );
