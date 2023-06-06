7913ad0aca1309165ee1ccbba483b4c4af79b70d
Dextergoff <dextergoff571@gmail.com>
2023-06-05 18:42:49 -0700

    display follower and following counts

--- frontend/client/src/containers/profile/InfoBar.js
+++ frontend/client/src/containers/profile/InfoBar.js
@@ -1,11 +1,11 @@
-const InfoBar = ({ data }) => {
+const InfoBar = ({ requested_user }) => {
   return (
     <div className="d-flex gap-5 mt-4 mb-3 justify-content-center">
       <div className="h6 align-self-center text-light fw-light">
-        0 followers
+        Followers {requested_user.followers_count}
       </div>
       <div className="h6 align-self-center text-light fw-light">
-        0 following
+        Following {requested_user.following_count}
       </div>
     </div>
   );
