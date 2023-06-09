575b0b01d996d727cfd634a3a5618f314b7382e4
Dextergoff <dextergoff571@gmail.com>
2023-06-09 12:58:46 -0700

    add following and hashtags post endpoints

--- app/posts/urls.py
+++ app/posts/urls.py
@@ -3,8 +3,12 @@ from .views import *
 app_name = "posts"
 urlpatterns = [
     path('post/<pk>', PostsView.as_view({'get': 'main'}), ),
-    path('page/<int:page>/',
-         PageView.as_view({'get': 'main', 'post': 'main'}), ),
+    path('discover/<int:page>/',
+         DiscoverView.as_view({'get': 'main', 'post': 'main'}), ),
+    path('following/<int:page>/<int:pk>',
+         FollowingView.as_view({'get': 'main', 'post': 'main'}), ),
+    path('hashtag/<int:page>/',
+         HashTagView.as_view({'get': 'main', 'post': 'main'}), ),
     path('likepost/', LikePost.as_view({'post': 'like'}), ),
     path('createpost/', CreatePostView.as_view(), ),
 ]
