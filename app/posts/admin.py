2491cf46b6c2ea8d4f480fcb2414a9133be32b51
Dextergoff <dextergoff571@gmail.com>
2023-06-12 13:18:04 -0700

    remove useless admin

--- app/posts/admin.py
+++ app/posts/admin.py
@@ -1,11 +1,5 @@
 from django.contrib import admin
 from .models import *
-class TestModelAdmin(admin.ModelAdmin):
-    def get_queryset(self, request):
-        test_model_qs = super(TestModelAdmin, self).get_queryset(request)
-        test_model_qs = test_model_qs.prefetch_related('many-to-many-field')
-        raw_id_fields = ("likes",)
-        return test_model_qs
-admin.site.register(Post, TestModelAdmin)
-admin.site.register(Image)
 
+admin.site.register(Post)
+admin.site.register(Image)
